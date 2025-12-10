3a079f9f52bbb07eb73576c40b24fe3f36b6edb8
Rahul Ray <rahulray21032003@gmail.com>
2025-12-10 17:04:16 +0530

    test 2

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // index.js
-// import 'dotenv/config';               // <- loads .env immediately
+import 'dotenv/config';               // <- loads .env immediately
 import express from "express";
-// import bodyParser from "body-parser";
-// import { fileURLToPath } from 'url';
-// import cors from "cors";
+import bodyParser from "body-parser";
+import { fileURLToPath } from 'url';
+import cors from "cors";
 
 
-// import routerFactory from "./routes/routes.js";
-// import aadhaarRoutes from "./routes/aadhaar.js";
-// import client from "./config/redis.js"; // config will use process.env now
-// import Connection from './db/db.js';
-// import kycRoutes from './routes/kycRoutes.js';
+import routerFactory from "./routes/routes.js";
+import aadhaarRoutes from "./routes/aadhaar.js";
+import client from "./config/redis.js"; // config will use process.env now
+import Connection from './db/db.js';
+import kycRoutes from './routes/kycRoutes.js';
 
-// import adminAuthRoutes from "./routes/adminAuthRoutes.js";
-// import adminKycRoutes from "./routes/adminKycRoutes.js";
+import adminAuthRoutes from "./routes/adminAuthRoutes.js";
+import adminKycRoutes from "./routes/adminKycRoutes.js";
 // // import kycRoutes from './routes/kyc.js';
 
 const app = express();
