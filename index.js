1d1d019ce676a5e31579ac947e8adf0e45d28558
Rahul Ray <rahulray21032003@gmail.com>
2025-12-10 17:03:21 +0530

    test 1

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 // index.js
-import 'dotenv/config';               // <- loads .env immediately
+// import 'dotenv/config';               // <- loads .env immediately
 import express from "express";
-import bodyParser from "body-parser";
-import { fileURLToPath } from 'url';
-import cors from "cors";
+// import bodyParser from "body-parser";
+// import { fileURLToPath } from 'url';
+// import cors from "cors";
 
 
-import routerFactory from "./routes/routes.js";
-import aadhaarRoutes from "./routes/aadhaar.js";
-import client from "./config/redis.js"; // config will use process.env now
-import Connection from './db/db.js';
-import kycRoutes from './routes/kycRoutes.js';
+// import routerFactory from "./routes/routes.js";
+// import aadhaarRoutes from "./routes/aadhaar.js";
+// import client from "./config/redis.js"; // config will use process.env now
+// import Connection from './db/db.js';
+// import kycRoutes from './routes/kycRoutes.js';
 
-import adminAuthRoutes from "./routes/adminAuthRoutes.js";
-import adminKycRoutes from "./routes/adminKycRoutes.js";
-// import kycRoutes from './routes/kyc.js';
+// import adminAuthRoutes from "./routes/adminAuthRoutes.js";
+// import adminKycRoutes from "./routes/adminKycRoutes.js";
+// // import kycRoutes from './routes/kyc.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors());
+// app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// routerFactory is a function that returns an express.Router configured with the passed client
-app.use("/", routerFactory(client));
-app.use("/aadhaar", aadhaarRoutes);
-app.use("/uploads", express.static("uploads"));
-app.use("/kyc", kycRoutes);
+// // routerFactory is a function that returns an express.Router configured with the passed client
+// app.use("/", routerFactory(client));
+// app.use("/aadhaar", aadhaarRoutes);
+// app.use("/uploads", express.static("uploads"));
+// app.use("/kyc", kycRoutes);
 
-app.use("/admin/auth", adminAuthRoutes);
-app.use("/admin/kyc", adminKycRoutes);
+// app.use("/admin/auth", adminAuthRoutes);
+// app.use("/admin/kyc", adminKycRoutes);
 
 
 
-await Connection(process.env.DB_URI);
+// await Connection(process.env.DB_URI);
+
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
 
 app.listen(PORT, () => {
